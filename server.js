f2bad4eb3fe935345fe85939baa0da9190e2ce36
SSERTG <115524247+SSERTG@users.noreply.github.com>
2022-10-11 14:40:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const socket = require('socket.io')(http,{cors:{origin:"*"}});
+const socket = require('socket.io')
 
 const app = express()
 const server = app.listen(process.env.PORT || 3000, () => {
